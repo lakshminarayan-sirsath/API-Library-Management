c5a92468fa267c640fe6a56e13ac06baed51ae3d
Lakshminarayan Sirsath <sirsathls97@gmail.com>
2025-01-02 12:40:32 +0530

    Created controllers.

---
+++ library-management/src/main/java/com/task/library/management/RestController/BookController.java
@@ -0,0 +1,10 @@
+package com.task.library.management.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+}
