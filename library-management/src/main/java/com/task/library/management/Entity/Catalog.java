b51d231ddf7006ad24b5b5b42f3813b1bcf162da
Lakshminarayan Sirsath <sirsathls97@gmail.com>
2025-01-02 11:02:48 +0530

    Added one-to-one relationship between catalog and book where catalog contain book_id

--- library-management/src/main/java/com/task/library/management/Entity/Catalog.java
+++ library-management/src/main/java/com/task/library/management/Entity/Catalog.java
@@ -1,10 +1,13 @@
 package com.task.library.management.Entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,5 +37,8 @@ public class Catalog {
 	private String availabilityStatus;
 	
 	// BookID (Foreign Key)
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
 
 }
