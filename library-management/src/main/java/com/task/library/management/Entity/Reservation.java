64d80207dfcda19ba92830241e731ca6592be574
Lakshminarayan Sirsath <sirsathls97@gmail.com>
2025-01-02 11:41:56 +0530

    Added many-to-one relationship between (Reservation and Book) and (Reservation and Member) [uni-dir].

--- library-management/src/main/java/com/task/library/management/Entity/Reservation.java
+++ library-management/src/main/java/com/task/library/management/Entity/Reservation.java
@@ -2,11 +2,15 @@ package com.task.library.management.Entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,5 +37,16 @@ public class Reservation {
 	private String status; // (e.g., Pending, Canceled, Completed)
 	
 	// BookID (Foreign Key)
+	// deleting reservation will not delete book.
+	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+	          CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
+	
 	// MemberID (Foreign Key)
+	// deleting reservation will not member book.
+	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+		      CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
 }
